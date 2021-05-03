24d794e3d6efb351aaf4fc192469dbfa00c76126
Juan <jd.berbeo@gmail.com>
2021-05-03 15:59:06 -0500

    cambio user-agent

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-   res.send('user-agent') 
+   res.send('mi-super-navegador') 
 })
 
 app.listen(3000, () => console.log('Server running in port 3000'));
